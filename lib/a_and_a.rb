4c05c802d2af78941457564b9dbe2496242d4a42
Jennifer Kinsey <kinseyplusplus@gmail.com>
2017-04-21 10:30:27 -0700

    adds test for case sensitivity and spacing

--- lib/a_and_a.rb
+++ lib/a_and_a.rb
@@ -3,7 +3,7 @@ class String
   def anagram? (input2)
     input1 = self.downcase
     input2.downcase!
-    both_words = (input1 + input2).split("")
-    both_words.uniq == input1.split("")
+    both_words = (input1 + input2).split("") - [" "]
+    both_words.uniq == (input1.split("") - [" "])
   end
 end
--- spec/a_and_a_spec.rb
+++ spec/a_and_a_spec.rb
@@ -5,4 +5,16 @@ describe("#String#anagram") do
   it("returns true if a one word string entered is the anagram of another String") do
     expect(("eat").anagram?("tea")).to(eq(true))
   end
+
+  it("returns false if a one word string entered is not the anagram of another String") do
+    expect(("honey").anagram?("oats")).to(eq(false))
+  end
+
+  it("returns true for anagrams of different cases") do
+    expect(("eAT").anagram?("Tea")).to(eq(true))
+  end
+
+  it("returns true for anagrams with different spacing") do
+    expect(("e A T").anagram?("Te  a")).to(eq(true))
+  end
 end
